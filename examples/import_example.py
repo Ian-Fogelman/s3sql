ed24c3ff33a25319141120466a76c78daf782e68
Ian-Fogelman <IanFogelman@gmail.com>
2025-07-07 21:37:43 -0400

    tweak example

--- examples/import_example.py
+++ examples/import_example.py
@@ -6,15 +6,14 @@ s3sql query --uri "s3://s3sql-demo/sql_engines.csv" --sql "SELECT * FROM df" --o
 ```
 """
 
-import s3sql
-import s3sql.cli
+import s3sql.cli as s3sql
 import os
 
 uri = 's3://s3sql-demo/sql_engines.csv' #todo: change to your bucket/file path
 sql = 'SELECT * from df'
 out = 'output.csv'
 
-s3sql_client = s3sql.cli.S3SQLClient()
+s3sql_client = s3sql.S3SQLClient()
 #s3sql_client.set_key(os.getenv('AWS_ACCESS_KEY_ID')) #todo: set to env variable with access key.
 #s3sql_client.set_secret(os.getenv('AWS_SECRET_ACCESS_KEY')) #todo: set to env variable with access secret.
 s3sql_client.query(uri, sql, out)
